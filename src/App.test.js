80ce0b2e7ebdda09ff3872391b3856fc51d4acda
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-03 14:39:10 -0800

    fixed typo

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', async() => {
+test('renders learn react link', async () => {
   render(<App />);
   const linkElement = await screen.findByText(/jiji/i);
   expect(linkElement).toBeInTheDocument();
